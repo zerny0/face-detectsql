8b7b65874f55d8fdeb85dbcbab0b246596f72207
diyasss_0 <diasamanbayuly@mail.ru>
2026-06-12 07:55:21 +0500

    Create facedet-by-img.py

---
+++ facedet-by-img.py
@@ -0,0 +1,31 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades +
+    'haarcascade_frontalface_default.xml'
+)
+
+image = cv2.imread('sample.jpg')
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5
+)
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(
+        image,
+        (x, y),
+        (x+w, y+h),
+        (255, 0, 0),
+        2
+    )
+
+print("Табылған бет саны:", len(faces))
+
+cv2.imshow("Face Detection", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
